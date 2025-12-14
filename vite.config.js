7d8b1074014378f4bff6bd0ec8ac696fc651ec46
Federico Lanzafame <30768678+sprynkler@users.noreply.github.com>
2025-12-14 23:57:06 +0100

    Set base path for Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/TestRepo/',
   plugins: [react({
     babel: {
       plugins: [['babel-plugin-react-compiler']],

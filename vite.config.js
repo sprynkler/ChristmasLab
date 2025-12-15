3fe080dba3528299bfbf250069368ee097cc1d21
Federico Lanzafame <30768678+sprynkler@users.noreply.github.com>
2025-12-15 01:11:16 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/TestRepo/',
+  base: '/ChristmasLab/',
   plugins: [react({
     babel: {
       plugins: [['babel-plugin-react-compiler']],
